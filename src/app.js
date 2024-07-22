a3bbf2f919046ae6fecab2220fe1490cf46a297e
Roseedee Cehleah <49389805+Roseedee@users.noreply.github.com>
2024-07-22 22:32:55 +0700

    init project and setup some thing

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const port = 80;
+
+const app = express();
+
+app.set ("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "../node_modules/bootstrap/dist/")));
+app.use('/brand', express.static(path.join(__dirname, '../assets/brand'))); //cal /brand/<file name>
+app.use(express.static(path.join(__dirname, 'css')));
+
+app.get('/', (req, res) => {
+    res.render(path.join(__dirname, 'views'));
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})

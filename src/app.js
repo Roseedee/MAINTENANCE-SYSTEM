c452bc4d8c09efff6658ee0800db1992c795a076
Roseedee Cehleah <49389805+Roseedee@users.noreply.github.com>
2024-08-08 12:37:24 +0700

    update layout

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ app.set ("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "../node_modules/bootstrap/dist/")));
 app.use('/brand', express.static(path.join(__dirname, '../assets/brand'))); //cal /brand/<file name>
+app.use('/icons', express.static(path.join(__dirname, '../assets/icons'))); //cal /brand/<file name>
 app.use(express.static(path.join(__dirname, 'css')));
 
 app.get('/', (req, res) => {

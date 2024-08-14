b703d6c36e1c9dae779c1929d02846f439fe131c
Roseedee Cehleah <49389805+Roseedee@users.noreply.github.com>
2024-08-14 22:33:13 +0700

    update

--- src/route.js
+++ src/route.js
@@ -5,11 +5,15 @@ const path = require('path');
 const view_path = path.join(__dirname, 'views');
 
 route.get('/', (req, res) => {
-    res.render(view_path)
+    res.render(view_path, {
+        page: 'dashboard'
+    });
 })
 
 route.get('/search', (req, res) => {
-    res.render(path.join(view_path, 'search'))
+    res.render(path.join(view_path, 'search'), {
+        page: 'search'
+    });
 })
 
 module.exports = route;

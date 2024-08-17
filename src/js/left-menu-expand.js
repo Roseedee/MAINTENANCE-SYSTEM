3719327ea285f41fd5e0d59ffc4896cfec0ce53e
Roseedee Cehleah <49389805+Roseedee@users.noreply.github.com>
2024-08-17 13:26:28 +0700

    fix left menu can't click

--- src/js/left-menu-expand.js
+++ src/js/left-menu-expand.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $('.left-menu-item-expand').click(function(e) {
-    e.preventDefault();
+    // e.preventDefault();
     $(this).find('.left-menu-expand-header').toggleClass('expanded');
     $(this).find('.sub-left-menu-expand-container').slideToggle('fast');
     $(this).find('.icon-expand').toggleClass('rotate');

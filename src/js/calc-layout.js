1f04e62ddfa31a91eb62675d63842ffc2c4c7f24
Roseedee Cehleah <49389805+Roseedee@users.noreply.github.com>
2024-08-18 12:55:43 +0700

    update content responsive on browser resize

--- src/js/calc-layout.js
+++ src/js/calc-layout.js
@@ -6,10 +6,15 @@ function onLoad() {
           const header = document.getElementsByTagName('header')[0];
           const class_main = document.getElementsByClassName('main')[0];
           const class_content = document.getElementsByClassName('body')[0];
+          const class_sidebar_left = document.getElementsByClassName('sidebar-left')[0];
+
           const min_content_h = (window.innerHeight - header_h) + 'px';
 
           header.style.height = header_h + 'px';
           class_main.style.top = header_h + 'px';
           class_main.style.minHeight = min_content_h;
           class_content.style.minHeight = min_content_h;
+          class_sidebar_left.style.height = min_content_h
 }
+
+window.addEventListener('resize', onLoad);

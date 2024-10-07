ea27b2b35738c08428dd91f147c2b0091e9ab9f8
Roseedee Cehleah <49389805+Roseedee@users.noreply.github.com>
2024-10-07 07:42:17 +0700

    update calculate layout

--- src/js/calc-layout.js
+++ src/js/calc-layout.js
@@ -1,20 +1,30 @@
-const header_h = 60; //px
+const rootStyle = getComputedStyle(document.querySelector(':root'))
+
+const header_h = rootStyle.getPropertyValue('--header-h');
+const sidebar_left_w = rootStyle.getPropertyValue('--sidebar-left-w')
 
 onLoad();
 
+function getNumberFromValue(value) {
+    const number = value.match(/\d+/); // กรองเฉพาะตัวเลข
+    return number ? Number(number[0]) : 0; // แปลงเป็นตัวเลข ถ้าไม่มีตัวเลขจะคืนค่า null
+}
+
 function onLoad() {
-          const header = document.getElementsByTagName('header')[0];
-          const class_main = document.getElementsByClassName('main')[0];
-          const class_content = document.getElementsByClassName('body')[0];
-          const class_sidebar_left = document.getElementsByClassName('sidebar-left')[0];
 
-          const min_content_h = (window.innerHeight - header_h) + 'px';
+    const header = document.getElementsByTagName('header')[0];
+    const class_main = document.getElementsByClassName('main')[0];
+    const class_content = document.getElementsByClassName('body')[0];
+    const class_sidebar_left = document.getElementsByClassName('sidebar-left')[0];
 
-          header.style.height = header_h + 'px';
-          class_main.style.top = header_h + 'px';
-          class_main.style.minHeight = min_content_h;
-          class_content.style.minHeight = min_content_h;
-          class_sidebar_left.style.height = min_content_h
+    const min_content_h = (window.innerHeight - getNumberFromValue(header_h)) + 'px';
+
+    header.style.height = header_h;
+    class_main.style.top = header_h;
+    class_main.style.minHeight = min_content_h;
+    class_content.style.minHeight = min_content_h;
+    class_content.style.width = (window.innerWidth - getNumberFromValue(sidebar_left_w)) + 'px'
+    class_sidebar_left.style.height = min_content_h
 }
 
 window.addEventListener('resize', onLoad);
